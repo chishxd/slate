effad8013ea9576ea007bb7f056e088a849f591c
Chinmay Shet <chishxd@gmail.com>
2025-12-21 17:24:09 +0530

    feat: Output files are stored with MD files

--- lib/slate.rb
+++ lib/slate.rb
@@ -2,10 +2,12 @@
 
 require_relative "slate/version"
 require "kramdown"
+require 'kramdown-parser-gfm'
 
 module Slate
   class Error < StandardError; end
   class CLI
+    # This method is the MAIN method... Refactored from bin/slate
     def self.run(path)
       path = File.expand_path(path)
       if File.file?(path)
@@ -18,7 +20,7 @@ module Slate
     end
 
     def self.convert(md_content, title)
-      raw_html = Kramdown::Document.new(md_content).to_html
+      raw_html = Kramdown::Document.new(md_content, input: 'GFM').to_html
 
       final_html = "<head>\n<title> #{title} </title> <link href='style.css' rel='stylesheet'></head><body class='markdown-body'>\n#{raw_html}\n</body>"
 
@@ -28,14 +30,15 @@ module Slate
     def self.process_file(path)
       puts "Argument seems to be a single file, Parsing it"
       content = File.read(path)
-      filename = File.basename(path, ".*")  
+      filename = File.basename(path, ".*")
       dir = File.dirname(path)
   
       style_path = File.join(__dir__, '..', 'style.css')
       FileUtils.cp(style_path, dir)
       
       final_html = convert(content, filename)
-      File.write("#{filename}.html", final_html)
+      output_path = File.join(path, "#{filename}.html")
+      File.write(output_path, final_html)
       puts "Saved #{filename}.html"
     end
 
@@ -50,7 +53,8 @@ module Slate
         content = File.read(file)
         filename = File.basename(file, ".*")    
         final_html = convert(content, file)
-        File.write("#{filename}.html", final_html)
+        output_path = File.join(path, "#{filename}.html")
+        File.write(output_path, final_html)
         puts "Saved #{filename}.html"
       end
     end
